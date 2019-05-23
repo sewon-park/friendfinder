2882dfab6b174d277c5023302fd6505d1ed902e2
won518 <sewonpark518@gmail.com>
2019-05-22 23:04:23 -0700

    edits to html-routs. added /

--- app/routes/htmlRoutes.js
+++ app/routes/htmlRoutes.js
@@ -9,4 +9,9 @@ module.exports = function(app) {
   app.get("*", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/home.html"));
   });
+
+  app.get("/", function(req, res) {
+    res.json(path.join(__dirname, "../public/home.html"));
+  });
+  
 };
