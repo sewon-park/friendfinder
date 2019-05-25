110fba90720179aec9834c1d2050e3d571fbb9d8
won518 <sewonpark518@gmail.com>
2019-05-24 21:25:05 -0700

    added more friends data

--- app/data/friendsData.js
+++ app/data/friendsData.js
@@ -1,36 +1,88 @@
 var friendsData = [
   {
-    "name":"Hawkeye",
-    "photo":"https://i.kinja-img.com/gawker-media/image/upload/s--qFwf2Dk4--/c_scale,f_auto,fl_progressive,q_80,w_800/e4u1njnr4shj3qoz4ncy.jpg",
-    "scores":[
-        1,
-        2,
-        3,
-        2,
-        4,
-        4,
-        4,
-        5,
-        4,
-        1
-      ]
+    "name": "Hawkeye",
+    "photo": "https://i.kinja-img.com/gawker-media/image/upload/s--qFwf2Dk4--/c_scale,f_auto,fl_progressive,q_80,w_800/e4u1njnr4shj3qoz4ncy.jpg",
+    "scores": [
+      1,
+      2,
+      3,
+      2,
+      4,
+      4,
+      4,
+      5,
+      4,
+      1
+    ]
   },
   {
-    "name":"Natalia Alianovna Romanova",
-    "photo":"https://am22.akamaized.net/tms/cnt/uploads/2018/05/black-widow-scarlett-johansson-marvel-the-avengers.jpg",
-    "scores":[
-        2,
-        3,
-        1,
-        3,
-        5,
-        2,
-        2,
-        5,
-        3,
-        2
-      ]
+    "name": "Natalia Alianovna Romanova",
+    "photo": "https://am22.akamaized.net/tms/cnt/uploads/2018/05/black-widow-scarlett-johansson-marvel-the-avengers.jpg",
+    "scores": [
+      2,
+      3,
+      1,
+      3,
+      5,
+      2,
+      2,
+      5,
+      3,
+      2
+    ]
+  },
+
+  {
+    "name": "Thor",
+    "photo": "https://d13ezvd6yrslxm.cloudfront.net/wp/wp-content/images/evolution-of-thor-5.png",
+    "scores": [
+      5,
+      5,
+      5,
+      4,
+      2,
+      4,
+      1,
+      3,
+      4,
+      2
+    ]
+  },
+  {
+    "name": "Steve Rogers",
+    "photo": "https://a.wattpad.com/cover/157422453-352-k923956.jpg",
+    "Scores": [],
+    "scores": [
+      1,
+      3,
+      2,
+      4,
+      4,
+      3,
+      2,
+      5,
+      4,
+      1
+    ]
+  },
+  {
+    "name": "Groot",
+    "photo": "https://cdn.vox-cdn.com/thumbor/xddVMltvqjB3PEgF4OnOQifZvZs=/0x0:1200x696/1200x800/filters:focal(535x82:727x274)/cdn.vox-cdn.com/uploads/chorus_image/image/58843699/baby-groot-guardians.0.0.jpg",
+    "Scores": [],
+    "scores": [
+      1,
+      4,
+      4,
+      1,
+      4,
+      2,
+      5,
+      2,
+      4,
+      2
+    ]
   }
+
 
 ];
 
